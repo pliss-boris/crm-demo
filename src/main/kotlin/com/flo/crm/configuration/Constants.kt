28b2a98f96aad209e20948be42f76a1587b2259b
Pliss Boris <pliss.boris@gmail.com>
2020-11-06 22:07:10 +0200

    Liquibase copleted with partitioned table

---
+++ src/main/kotlin/com/flo/crm/configuration/Constants.kt
@@ -0,0 +1,9 @@
+package com.flo.crm.configuration
+
+object Constants {
+    val maxLength = 128
+    object Data {
+        val minLen = 0
+        val maxLen = 128
+    }
+}
---
+++ src/main/kotlin/com/flo/crm/configuration/KafkaConfiguration.kt
@@ -0,0 +1,5 @@
+package com.flo.crm.configuration
+
+class KafkaConfiguration {
+    val len = Constants.Data.maxLen
+}
